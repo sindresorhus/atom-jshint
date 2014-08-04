07b96ba79df1ca2684301f72eb33bc7603c5af75
Payton Swick <payton@foolord.com>
2014-08-03 23:21:58 -0400

    Add markers to track error lines.

--- index.js
+++ index.js
@@ -6,31 +6,98 @@ var jsxhint = require('jshint-jsx').JSXHINT;
 var loadConfig = require('./load-config');
 var plugin = module.exports;
 
+var markersByEditorId = {};
+
 Subscriber.extend(plugin);
 
+function getMarkersForEditor() {
+	var editor = atom.workspace.getActiveEditor();
+	if ( editor && markersByEditorId[ editor.id ] ) {
+		return markersByEditorId[ editor.id ];
+	}
+	return {};
+}
+
+function clearOldMarks(errors) {
+	var rows = _.map(errors, function(error) {
+		return getRowForError(error);
+	});
+
+	var oldMarkers = getMarkersForEditor();
+	_.each(_.keys(oldMarkers), function(row) {
+		if ( ! _.contains(rows, row) ) {
+			destroyMarkerAtRow(row);
+		}
+	});
+}
+
+function destroyMarkerAtRow(row) {
+	var editor = atom.workspace.getActiveEditor();
+	if ( markersByEditorId[editor.id] && markersByEditorId[editor.id][row] ) {
+		markersByEditorId[editor.id][row].destroy();
+		delete markersByEditorId[editor.id][row];
+	}
+}
+
+function saveMarker(marker, row) {
+	var editor = atom.workspace.getActiveEditor();
+	if ( ! markersByEditorId[editor.id] ) {
+		markersByEditorId[editor.id] = {};
+	}
+	markersByEditorId[editor.id][row] = marker;
+}
+
+function getMarkerAtRow(row) {
+	var editor = atom.workspace.getActiveEditor();
+	if ( ! markersByEditorId[editor.id] ) {
+		return null;
+	}
+	return markersByEditorId[editor.id][row];
+}
+
+function markRow(row) {
+	var editor = atom.workspace.getActiveEditor();
+	if ( getMarkerAtRow(row) ) {
+		return;
+	}
+	var marker = editor.markBufferRange([[row, 0], [row, 1]]);
+	editor.decorateMarker(marker, {type: 'line', class: 'jshint-line'});
+	editor.decorateMarker(marker, {type: 'gutter', class: 'jshint-line-number'});
+	saveMarker(marker, row);
+}
+
 function updateStatusbar(error) {
+	if ( ! error ) {
+		return;
+	}
+	error = error[0];
 	if (atom.workspaceView.statusBar) {
 		atom.workspaceView.statusBar.appendLeft('<span id="jshint-statusbar" class="inline-block">JSHint ' + error.line + ':' + error.character + ' ' + error.reason + '</span>');
 	}
 }
 
-function displayError(error, editor, editorView) {
+function getRowForError(error) {
 	var line = error[0].line || 1; // JSHint reports `line: 0` when config error
 	var row = line - 1;
-	var gutter = editorView.gutter;
-	var bufferRange = editor.bufferRangeForBufferRow(row);
-	bufferRange.start.column = bufferRange.end.column = error.character;
-	var screenRange = editor.screenRangeForBufferRange(bufferRange);
-	var lineEl = editorView.lineElementForScreenRow(screenRange.start.row);
-	lineEl.addClass('jshint-line');
+	return row;
+}
 
+function displayError(error) {
+	var row = getRowForError(error);
+	markRow(row);
+	addReasons(error);
+}
+
+function addReasons(error) {
+	var row = getRowForError(error);
+	var editorView = atom.workspaceView.getActiveView();
+	var gutter = editorView.gutter;
 	var reasons = _.map(error, function (el) {
 		return el.character + ': ' + el.reason;
 	}).join('\n\n');
 
 	var gutterRow = gutter.find(gutter.getLineNumberElement(row));
 	gutterRow.attr('title', reasons);
-	gutterRow.addClass('jshint-line-number');
 }
 
 function lint() {
@@ -49,8 +116,7 @@ function lint() {
 	var config = file ? loadConfig(file) : {};
 
 	// reset
-	editorView.resetDisplay();
-	editorView.gutter.find('.jshint-line-number').removeClass('jshint-line-number');
+	editorView.gutter.find('.jshint-line-number').attr('title', '');
 
 	if (atom.workspaceView.statusBar) {
 		atom.workspaceView.statusBar.find('#jshint-statusbar').remove();
@@ -62,34 +128,32 @@ function lint() {
 	// workaround the errors array sometimes containing `null`
 	var errors = _.compact(linter.errors);
 
-	if (errors.length === 0) {
-		return;
+	if (errors.length > 0) {
+		// aggregate same-line errors
+		var ret = [];
+		_.each(errors, function (el) {
+			var l = el.line;
+
+			if (Array.isArray(ret[l])) {
+				ret[l].push(el);
+
+				ret[l] = _.sortBy(ret[l], function (el) {
+					return el.character;
+				});
+			} else {
+				ret[l] = [el];
+			}
+		});
+		errors = _.compact(ret);
 	}
 
-	var ret = [];
+	displayErrors(errors);
+}
 
-	// aggregate same-line errors
-	_.each(errors, function (el) {
-		var l = el.line;
-
-		if (Array.isArray(ret[l])) {
-			ret[l].push(el);
-
-			ret[l] = _.sortBy(ret[l], function (el) {
-				return el.character;
-			});
-		} else {
-			ret[l] = [el];
-		}
-	});
-
-	_.chain(ret).compact().each(function (error, i) {
-		if (i === 0) {
-			updateStatusbar(error[0]);
-		}
-
-		displayError(error, editor, editorView);
-	});
+function displayErrors(errors) {
+	clearOldMarks(errors);
+	updateStatusbar(_.first(errors));
+	_.each(errors, displayError);
 }
 
 function registerEvents() {
